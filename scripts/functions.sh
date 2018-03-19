bc48ae5b1c5b6d8ab87f10f6b0be4ad3d1751871
tamnv <tam.nguyen@ffwagency.com>
2018-03-19 18:43:40 +0700

    BE: Update script

--- scripts/functions.sh
+++ scripts/functions.sh
@@ -15,7 +15,9 @@ rsync_repos() {
 	echo -e "\n${txtylw}Prepare upstream repo for multidev${txtrst}"
 	rm -rf $HOME/acquia/config
 	rm -rf $HOME/acquia/docroot
+  rm -rf $HOME/acquia/hooks
 
 	rsync -ar $CIRCLE_WORKING_DIRECTORY/docroot/ $HOME/acquia/docroot/
 	rsync -ar $CIRCLE_WORKING_DIRECTORY/config/ $HOME/acquia/config/
+  rsync -ar $CIRCLE_WORKING_DIRECTORY/hooks/ $HOME/acquia/hooks/
 }
