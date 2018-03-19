4b907ae76df61511aa550d506de6c04f96c79383
tamnv <tam.nguyen@ffwagency.com>
2018-03-19 15:37:39 +0700

    [BE] Update circle config

---
+++ scripts/deploy_to_acquia.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+cd scripts
+source functions.sh
+echo -e "\n${txtgrn}Pushing the master branch to Github Upstream ${txtrst}"
+prepare_acquia_folder
---
+++ scripts/functions.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+prepare_acquia_folder() {
+  cd $HOME
+
+  # Prepare pantheon repo folder.
+  if [ ! -d "$HOME/acquia" ]; then
+    git clone "$ACQUIA_REPO" acquia
+  fi
+
+  cd $HOME/acquia
+  git fetch
+  git pull origin master
+}
