b6dd9b040f7e2f555b22fea9cb8141b9a1effc9b
tamnv <tam.nguyen@ffwagency.com>
2018-03-19 18:15:10 +0700

    [BE] Check deploy to acquia

--- scripts/deploy_to_acquia.sh
+++ scripts/deploy_to_acquia.sh
@@ -4,3 +4,8 @@ cd scripts
 source functions.sh
 echo -e "\n${txtgrn}Pushing the master branch to Github Upstream ${txtrst}"
 prepare_acquia_folder
+
+cd $HOME/acquia
+git add -A --force .
+git commit -m "Circle CI build a test message"
+git push -u origin master --force
--- scripts/functions.sh
+++ scripts/functions.sh
@@ -10,3 +10,12 @@ prepare_acquia_folder() {
   git fetch
   git pull origin master
 }
+
+rsync_repos() {
+	echo -e "\n${txtylw}Prepare upstream repo for multidev${txtrst}"
+	rm -rf $HOME/acquia/config
+	rm -rf $HOME/acquia/docroot
+
+	rsync -ar $CIRCLE_WORKING_DIRECTORY/docroot/ $HOME/acquia/docroot/
+	rsync -ar $CIRCLE_WORKING_DIRECTORY/config/ $HOME/acquia/config/
+}
