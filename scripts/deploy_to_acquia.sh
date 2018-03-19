d970feb60621f21303fe0b2e0dc58a7cb4036430
tamnv <tam.nguyen@ffwagency.com>
2018-03-19 18:22:12 +0700

    [BE] Test message 1

--- scripts/deploy_to_acquia.sh
+++ scripts/deploy_to_acquia.sh
@@ -6,6 +6,8 @@ echo -e "\n${txtgrn}Pushing the master branch to Github Upstream ${txtrst}"
 prepare_acquia_folder
 
 cd $HOME/acquia
+git config --global user.email $GIT_EMAIL
+git config --global user.name $GIT_USERNAME
 git add -A --force .
 git commit -m "Circle CI build a test message"
 git push -u origin master --force
