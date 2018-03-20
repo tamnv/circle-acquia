a1ce2b574fd7c0355b6b7a22c63fc4fd20f87702
tamnv <tam.nguyen@ffwagency.com>
2018-03-20 18:11:30 +0700

    [BE] Update script

--- scripts/functions.sh
+++ scripts/functions.sh
@@ -16,8 +16,12 @@ rsync_repos() {
 	rm -rf $HOME/acquia/config
 	rm -rf $HOME/acquia/docroot
   rm -rf $HOME/acquia/hooks
+  rm -rf $HOME/vendor
+  rm -f $HOME/acquia/load.environment.php
 
 	rsync -ar $CIRCLE_WORKING_DIRECTORY/docroot/ $HOME/acquia/docroot/
 	rsync -ar $CIRCLE_WORKING_DIRECTORY/config/ $HOME/acquia/config/
   rsync -ar $CIRCLE_WORKING_DIRECTORY/hooks/ $HOME/acquia/hooks/
+  rsync -ar $CIRCLE_WORKING_DIRECTORY/vendor/ $HOME/acquia/vendor/
+  cp $CIRCLE_WORKING_DIRECTORY/load.environment.php .
 }
