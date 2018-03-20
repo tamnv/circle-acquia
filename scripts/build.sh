3a66a9b25d3045d843d3baec22846f3f40e5798d
tamnv <tam.nguyen@ffwagency.com>
2018-03-20 17:52:39 +0700

    Update script

--- scripts/build.sh
+++ scripts/build.sh
@@ -13,16 +13,15 @@ txtrst='\033[0m' # Text reset.
 export COMPOSER_DISCARD_CHANGES=1
 export COMPOSER_NO_INTERACTION=1
 
-ls
-# if [ ! -f "$JSON" ]
-# then
-#   echo ${txtred}Error: No $JSON found ${txtrst}
-#   exit 1
-# fi
+if [ ! -f "$JSON" ]
+then
+  echo ${txtred}Error: No $JSON found ${txtrst}
+  exit 1
+fi
 
-# FOUND=`which $EXE`
-# COMPOSER_PARMS="--no-ansi --no-dev --no-interaction --optimize-autoloader --no-progress --prefer-dist"
+FOUND=`which $EXE`
+COMPOSER_PARMS="--no-ansi --no-dev --no-interaction --optimize-autoloader --no-progress --prefer-dist"
 
-# # Install Composer dependencies
-# echo -e "\n${txtylw}Invoking: $FOUND install $COMPOSER_PARMS ${txtrst}"
-# $FOUND install $COMPOSER_PARMS
+# Install Composer dependencies
+echo -e "\n${txtylw}Invoking: $FOUND install $COMPOSER_PARMS ${txtrst}"
+$FOUND install $COMPOSER_PARMS
