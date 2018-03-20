b77c6798474e3bb256cce399650e31bf178dd886
tamnv <tam.nguyen@ffwagency.com>
2018-03-20 17:44:50 +0700

    Debug

--- scripts/build.sh
+++ scripts/build.sh
@@ -13,6 +13,8 @@ txtrst='\033[0m' # Text reset.
 export COMPOSER_DISCARD_CHANGES=1
 export COMPOSER_NO_INTERACTION=1
 
+ls -l
+
 if [ ! -f "$JSON" ]
 then
   echo ${txtred}Error: No $JSON found ${txtrst}
